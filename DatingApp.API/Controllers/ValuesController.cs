964b8c5d69a6eac0912fa3ebb7406f50f1ffac4b
anowakowski <nowakowski.add@gmail.com>
2018-10-31 10:50:48 +0100

    test2

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -33,8 +33,7 @@ namespace DatingApp.API.Controllers
         [HttpGet("{id}")]
         public async Task<IActionResult> GetValue(int id)
         {
-            var value = await this.dataContext
-            .Values.FirstOrDefaultAsync(x => x.Id == id);
+            var value = await this.dataContext.Values.FirstOrDefaultAsync(x => x.Id == id);
 
             return Ok(value);
         }
