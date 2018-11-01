c4df631350622c36aeee50122bdd1b8028b0fd4e
Adrian Nowakowski <naad@MacBook-Pro-Adrian.local>
2018-11-01 23:00:46 +0100

    prepared login and loguat method

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -9,13 +10,28 @@ export class NavComponent implements OnInit {
 
   model: any = {};
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
-  login(){
-    console.log(this.model)
+  login() {
+    this.authService.login(this.model).subscribe(next => {
+      console.log('log succesfully');
+    }, error => {
+      console.log('faild to login');
+    });
   }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    console.log('logged out');
+  }
+
 
 }
