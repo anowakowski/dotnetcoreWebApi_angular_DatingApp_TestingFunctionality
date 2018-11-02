30430fb31fa3adcafb17c29586f1c3d65853f0cb
Adrian Nowakowski <naad@MacBook-Pro-Adrian.local>
2018-11-02 09:42:53 +0100

    added output prop to cancel register

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-register',
@@ -8,6 +8,8 @@ import { Component, OnInit, Input } from '@angular/core';
 export class RegisterComponent implements OnInit {
 
   @Input() valuesFromHome: any;
+  @Output() cancelRegister = new EventEmitter();
+
   model: any = {};
 
   constructor() { }
@@ -20,7 +22,7 @@ export class RegisterComponent implements OnInit {
   }
 
   cancel() {
-    console.log('cancel');
+    this.cancelRegister.emit(false);
   }
 
 }
