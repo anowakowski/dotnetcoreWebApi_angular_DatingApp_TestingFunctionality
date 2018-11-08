b55aa7edd2904e447d666a6865441b487bfab29b
anowakowski <nowakowski.add@gmail.com>
2018-11-08 11:20:18 +0100

    create resolver for meber details and list.

--- DatingApp-SPA/src/app/app-routing.module.ts
+++ DatingApp-SPA/src/app/app-routing.module.ts
@@ -6,6 +6,8 @@ import { MessagesComponent } from './messages/messages.component';
 import { ListsComponent } from './lists/lists.component';
 import { AuthGuard } from './_guards/auth.guard';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberDetailResolver } from './_resolves/member-detail.resolver';
+import { MemberListResolver } from './_resolves/member-list.resolver';
 
 export const appRoutes: Routes = [
   {path: '', component: HomeComponent},
@@ -14,10 +16,10 @@ export const appRoutes: Routes = [
     runGuardsAndResolvers: 'always',
     canActivate: [AuthGuard],
     children: [
-      {path: 'members', component: MemberListComponent},
-      {path: 'members/:id', component: MemberDetailComponent},
+      {path: 'members', component: MemberListComponent, resolve: {users: MemberListResolver}},
+      {path: 'members/:id', component: MemberDetailComponent, resolve: {user: MemberDetailResolver}},
       {path: 'messages', component: MessagesComponent},
-      {path: 'lists', component: ListsComponent}
+      {path: 'lists', component: ListsComponent }
     ]
   },
   {path: '**', redirectTo: '', pathMatch: 'full'}
