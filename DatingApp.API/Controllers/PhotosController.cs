1b201d9d15d36a9ce0f234651f2febd18c26106a
anowakowski <nowakowski.add@gmail.com>
2018-11-21 10:26:34 +0100

    api delete photo functionality

--- DatingApp.API/Controllers/PhotosController.cs
+++ DatingApp.API/Controllers/PhotosController.cs
@@ -142,5 +142,53 @@ namespace DatingApp.API.Controllers
 
             return Ok(photos);
         }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePhoto(int userId, int id)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+            {
+                return Unauthorized();
+            }
+
+            var user = await datingRepository.GetUser(userId);
+
+            if (!user.Photos.Any(x => x.Id == id))
+            {
+                return Unauthorized();
+
+            }
+
+            var photoFromRepo = await datingRepository.GetPhoto(id);
+
+            if (photoFromRepo.IsMain)
+            {
+                return BadRequest("you cannot delete your main photo");
+            }
+
+            if (photoFromRepo.PublicId != null)
+            {
+                var deleteParams = new DeletionParams(photoFromRepo.PublicId);
+                var result = cloudinary.Destroy(deleteParams);
+
+                if (result.Result == "ok")
+                {
+                    datingRepository.Delete(photoFromRepo);
+                }
+            }
+
+            if (photoFromRepo.PublicId == null)
+            {
+                datingRepository.Delete(photoFromRepo);
+            }
+
+            if (await datingRepository.SaveAll())
+            {
+                return Ok();
+            }
+
+            return BadRequest ("Faild to delete the photo");
+
+        }
     }
 }
