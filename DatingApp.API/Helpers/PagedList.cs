3ca0c96790c5a2c1452016889d3423db42b9aa12
anowakowski <nowakowski.add@gmail.com>
2018-12-05 15:04:17 +0100

    Added next part of pagination

--- DatingApp.API/Helpers/PagedList.cs
+++ DatingApp.API/Helpers/PagedList.cs
@@ -13,6 +13,7 @@ namespace DatingApp.API.Helpers
         public int TotalPages { get; set; }
         public int PageSize { get; set; }
         public int TotalCount { get; set; }
+
         public PagedList(List<T> items, int count, int pageNumber, int pageSize)
         {
             TotalCount = count;
@@ -22,11 +23,11 @@ namespace DatingApp.API.Helpers
             this.AddRange(items);
         }
 
-        public static async Task<PagedList<T>> CreateAsync(IQueryable<T> source, int pageNumber, int pageSize)
+        public static async Task<PagedList<T>> CreateAsync(IQueryable<T> source, 
+            int pageNumber, int pageSize)
         {
             var count = await source.CountAsync();
             var items = await source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
-
             return new PagedList<T>(items, count, pageNumber, pageSize);
         }
     }
--- DatingApp.API/Helpers/PaginationHeader.cs
+++ DatingApp.API/Helpers/PaginationHeader.cs
@@ -6,12 +6,13 @@ namespace DatingApp.API.Helpers
         public int ItemsPerPage { get; set; }
         public int TotalItems { get; set; }
         public int TotalPages { get; set; }
+
         public PaginationHeader(int currentPage, int itemsPerPage, int totalItems, int totalPages)
         {
             this.CurrentPage = currentPage;
             this.ItemsPerPage = itemsPerPage;
             this.TotalItems = totalItems;
-            this.TotalPages = TotalPages;
+            this.TotalPages = totalPages;
         }
 
         
