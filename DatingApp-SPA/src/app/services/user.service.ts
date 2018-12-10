8e73e3e8f729ca56fc636611e8c82c452734216d
anowakowski <nowakowski.add@gmail.com>
2018-12-10 16:16:17 +0100

    prepare filtering

--- DatingApp-SPA/src/app/services/user.service.ts
+++ DatingApp-SPA/src/app/services/user.service.ts
@@ -14,7 +14,7 @@ export class UserService {
 
   constructor(private http: HttpClient) { }
 
-  getUsers(page?, itemsPerPage?, userParams?): Observable<PaginationResult<User[]>> {
+  getUsers(page?, itemsPerPage?, userParams?, orderBy?): Observable<PaginationResult<User[]>> {
     const paginatedResult: PaginationResult<User[]> = new PaginationResult<User[]>();
 
     let params = new HttpParams();
@@ -28,7 +28,9 @@ export class UserService {
       params = params.append('minAge', userParams.minAge);
       params = params.append('maxAge', userParams.maxAge);
       params = params.append('gender', userParams.gender);
+      params = params.append('orderBy', userParams.orderBy);
     }
+
 
     return this.http.get<User[]>(this.baseUrl + 'users', {observe: 'response', params})
       .pipe (
